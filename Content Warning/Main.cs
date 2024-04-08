e0739fd8cb8de40ea8a00baa62a0238719e3ccff
Sinnisterly <60723887+Sinnisterly@users.noreply.github.com>
2024-04-07 20:29:48 -0700

    Changes
    Added new features. Re-arranged some.
    Edit 1.0.0.1

--- Content Warning/Main.cs
+++ Content Warning/Main.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Media.Media3D;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Steamworks;
 using UnityEngine.UI;
@@ -22,9 +23,17 @@ namespace ContentWarningHax
 
     class Main : MonoBehaviour
     {
-    
+        //not working dev console. Doesn't hurt anything either though.
+        public static bool developerConsoleEnabled
+        {
+            [MethodImpl(MethodImplOptions.InternalCall)]
+            get;
+            [MethodImpl(MethodImplOptions.InternalCall)]
+            set;
+        }
 
-      
+
+
         bool esp = true;
         bool TransformMovement = false;
         public string customText = ""; // Custom text input field
@@ -102,33 +111,37 @@ namespace ContentWarningHax
 
             // Create toggle buttons for each tab
             GUILayout.BeginVertical(GUILayout.Width(100));
-            if (GUILayout.Toggle(tab == 0, "Main", "Button", GUILayout.ExpandWidth(true)))
+            if (GUILayout.Toggle(tab == 0, "Main Tab", "Button", GUILayout.ExpandWidth(true)))
             {
                 tab = 0;
             }
-            if (GUILayout.Toggle(tab == 1, "Esp", "Button", GUILayout.ExpandWidth(true)))
+            if (GUILayout.Toggle(tab == 1, "ESP Tab", "Button", GUILayout.ExpandWidth(true)))
             {
                 tab = 1;
             }
-            if (GUILayout.Toggle(tab == 2, "Items", "Button", GUILayout.ExpandWidth(true)))
+            if (GUILayout.Toggle(tab == 2, "Item Spawner", "Button", GUILayout.ExpandWidth(true)))
             {
                 tab = 2;
             }
-            if (GUILayout.Toggle(tab == 3, "Monsters", "Button", GUILayout.ExpandWidth(true)))
+            if (GUILayout.Toggle(tab == 3, "Monster Spawner", "Button", GUILayout.ExpandWidth(true)))
             {
                 tab = 3;
             }
-            if (GUILayout.Toggle(tab == 4, "Steam", "Button", GUILayout.ExpandWidth(true)))
+            if (GUILayout.Toggle(tab == 4, "Steam Tab", "Button", GUILayout.ExpandWidth(true)))
             {
                 tab = 4;
             }
-            if (GUILayout.Toggle(tab == 5, "Random", "Button", GUILayout.ExpandWidth(true)))
+            if (GUILayout.Toggle(tab == 5, "Random Tab", "Button", GUILayout.ExpandWidth(true)))
             {
                 tab = 5;
             }
-            if (GUILayout.Toggle(tab == 6, "Players", "Button", GUILayout.ExpandWidth(true)))
+            if (GUILayout.Toggle(tab == 6, "Player Tab", "Button", GUILayout.ExpandWidth(true)))
             {
                 tab = 6;
+            }
+            if (GUILayout.Toggle(tab == 7, "Credits Tab", "Button", GUILayout.ExpandWidth(true)))
+            {
+                tab = 7;
             }
             GUILayout.EndVertical();
 
@@ -150,31 +163,39 @@ namespace ContentWarningHax
                     GUILayout.BeginHorizontal();
                     GUILayout.BeginVertical();
                 
-                    if (GUILayout.Button("Crasher"))
-                    {
-                        foreach (Player player in PlayerController)
-                        {
+                   
 
-                            if (!player.IsLocal)
-                            {
-                                PhotonNetwork.SetMasterClient(PhotonNetwork.LocalPlayer);
-                                PhotonNetwork.DestroyPlayerObjects(player.refs.view.Controller);
-                            }
-
-                        }
-
-
-                    }
-                    if (GUILayout.Button("Call Revive"))
+                    if (GUILayout.Button("Revive (self)"))
                     {
 
                         Player.localPlayer.CallRevive();
                     }
-                    if (GUILayout.Button("Set Host"))
-                    {
+                   
 
-                        PhotonNetwork.SetMasterClient(PhotonNetwork.LocalPlayer);
+                    if (GUILayout.Button("Recharge Battery"))
+                    {
+                        PlayerInventory playerInventory;
+                        Player.localPlayer.TryGetInventory(out playerInventory);
+                        foreach (InventorySlot inventorySlot in playerInventory.slots)
+                        {
+                            BatteryEntry batteryEntry;
+                            if (inventorySlot.ItemInSlot.item != null && inventorySlot.ItemInSlot.data.TryGetEntry<BatteryEntry>(out batteryEntry) && batteryEntry.m_maxCharge > batteryEntry.m_charge)
+                            {
+                                batteryEntry.AddCharge(10000);
+                            }
+                        }
                     }
+
+                   if (GUILayout.Button("Infinite Oxygen"))
+                    {
+                        Player.localPlayer.data.usingOxygen = false;
+                    }
+
+                    if (GUILayout.Button("Infinite Stamina"))
+                    {
+                        Player.localPlayer.data.staminaDepleated = false;
+                    }
+
 
 
                     GUILayout.EndVertical();
@@ -198,23 +219,23 @@ namespace ContentWarningHax
                         }
 
                     }
-                    if (GUILayout.Button("BotHandler"))
-                    {
-                        BotHandler.instance.DestroyAll();
-
-                    }
-                    if (GUILayout.Button("Max Stats"))
-                    {
-                            Player.localPlayer.data.remainingOxygen = float.MaxValue;
-                            Player.localPlayer.data.maxOxygen = float.MaxValue;
-                            Player.localPlayer.data.health = float.MaxValue;
-                            Player.localPlayer.data.currentStamina = float.MaxValue;
-                            Player.localPlayer.data.rested = true;
-
-                        
-                    }
                   
-                    if (GUILayout.Button("Kill All"))
+                    if (GUILayout.Button("Fake Fail Quota"))
+                    {
+                        if (PhotonNetwork.IsMasterClient)
+                        {                            
+                            FindObjectOfType<EndGameScreen>().StartWatching();
+                            WaitForSeconds waitForSeconds = new WaitForSeconds(10f);
+                            for (int i = 0; i < PlayerHandler.instance.playerAlive.Count; i++)
+                            {
+                                PlayerHandler.instance.playerAlive[i].Die();
+                            }
+                        }
+                    }
+
+
+
+                    if (GUILayout.Button("Kill Self"))
                     {
                         for (int i = 0; i < PlayerHandler.instance.playerAlive.Count; i++)
                         {
@@ -321,16 +342,83 @@ namespace ContentWarningHax
                         MainMenuHandler.Instance.OnCreatedRoom();
 
                     }
+                    if (GUILayout.Button("Set Host"))
+                    {
+
+                        PhotonNetwork.SetMasterClient(PhotonNetwork.LocalPlayer);
+                    }
+
                     GUILayout.EndVertical();
                     break;
                 case 5:
                     GUILayout.BeginVertical(GUI.skin.box);
-                    if (GUILayout.Button("StartMoney<host>"))
+                    if (GUILayout.Button("Lots of Start Money (host)"))
                     {
-                        BigNumbers.Instance.StartMoney = int.MaxValue;
+                        BigNumbers.Instance.StartMoney = 99999999;
 
                     }
-                  
+
+                    if (GUILayout.Button("Add 10k Money (host)"))
+                    {
+                        SurfaceNetworkHandler.RoomStats.AddMoney(10000);
+
+                    }
+
+                    if (GUILayout.Button("Remove 10k Money (host)"))
+                    {
+                        SurfaceNetworkHandler.RoomStats.RemoveMoney(10000);
+
+                    }
+
+                    if (GUILayout.Button("Add 10k to Views"))
+                    {
+                        SurfaceNetworkHandler.AddQuota(1000);
+                    }
+
+                    if (GUILayout.Button("BotHandler"))
+                    {
+                        BotHandler.instance.DestroyAll();
+
+                    }
+
+
+                    if (GUILayout.Button("Max Stats"))
+                    {
+                        Player.localPlayer.data.remainingOxygen = float.MaxValue;
+                        Player.localPlayer.data.maxOxygen = float.MaxValue;
+                        Player.localPlayer.data.health = float.MaxValue;
+                        Player.localPlayer.data.currentStamina = float.MaxValue;
+                        Player.localPlayer.data.rested = true;
+
+
+                    }
+
+                    if (GUILayout.Button("Clear Inventory"))
+                    {
+                        PlayerInventory playerInventory;
+                        Player.localPlayer.TryGetInventory(out playerInventory);
+                        foreach (InventorySlot inventorySlot in playerInventory.slots)
+                        {
+                            playerInventory.Clear();
+                        }
+                    }
+
+                    if (GUILayout.Button("Crash"))
+                    {
+                        foreach (Player player in PlayerController)
+                        {
+
+                            if (!player.IsLocal)
+                            {
+                                PhotonNetwork.SetMasterClient(PhotonNetwork.LocalPlayer);
+                                PhotonNetwork.DestroyPlayerObjects(player.refs.view.Controller);
+                            }
+
+                        }
+
+
+                    }
+
                     GUILayout.EndVertical();
 
                     break;
@@ -358,6 +446,17 @@ namespace ContentWarningHax
 
                     break;
 
+
+            }
+            switch (tab)
+            { 
+            case 7:
+                GUILayout.BeginVertical(GUI.skin.box);
+                GUILayout.Label("Credits:");
+                GUILayout.Label("Sinnisterly (added things)- https://github.com/Sinnisterly ");
+                GUILayout.Label("WoodgamerHD (for base/source)- https://github.com/WoodgamerHD ");
+                GUILayout.EndVertical();
+                break;
             }
 
             GUILayout.EndVertical();
@@ -390,7 +489,8 @@ namespace ContentWarningHax
             }
         }
     
-       
+
+
         public static void SpawnMonster(string monster)
         {
             RaycastHit raycastHit = HelperFunctions.LineCheck(MainCamera.instance.transform.position, MainCamera.instance.transform.position + MainCamera.instance.transform.forward * 30f, HelperFunctions.LayerType.TerrainProp, 0f);
@@ -413,7 +513,7 @@ namespace ContentWarningHax
                 // Set the background color
                 GUI.backgroundColor = backgroundColor;
 
-                windowRect = GUI.Window(0, windowRect, MenuWindow, "WoodSDK(Do not Resell or repost From unknowncheats.me)"); // Create the window with title "Menu"
+                windowRect = GUI.Window(0, windowRect, MenuWindow, "WoodSDK - edited @ Sinnisterly"); // Create the window with title "Menu"
              
                 
             }
@@ -492,9 +592,9 @@ namespace ContentWarningHax
             // Center the window on the screen
             windowRect.x = (Screen.width - windowRect.width) / 2;
             windowRect.y = (Screen.height - windowRect.height) / 2;
+            // Attempt to enable the developer console. !NOT WORKING! but doesn't effect anything
+            developerConsoleEnabled = true;
 
-
-        
 
         }
 
@@ -510,7 +610,7 @@ namespace ContentWarningHax
             {
                 showMenu = !showMenu;
             }
-         
+
 
             natNextUpdateTime += Time.deltaTime;
 
